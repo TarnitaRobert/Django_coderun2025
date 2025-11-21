99420a86cfd546e2e7e893e4d97a1e648ad9546a
Robert <tarnita.robert01@gmail.com>
2025-11-21 10:20:01 +0200

    added demo models

--- coderun2025_django/demo_app/models.py
+++ coderun2025_django/demo_app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+# demo model class, user info based
+class UserInfo(models.Model):
+    name= models.CharField(max_length=32)
+    surname = models.CharField(max_length=32)
+    age = models.IntegerField()
+    mail = models.EmailField(max_length=254)
+    job = models.CharField(max_length=32)
+    date = models.DateTimeField(auto_now_add=True)
+    slug = models.SlugField()
+
+    def __str__(self):
+        return self.name
+
+class DemoModel(models.Model):
+    field1= models.CharField(max_length=32)
+    field2= models.FloatField()
+
+    def __str__(self):
+        return self.field1
+
--- coderun2025_django/demo_app/views.py
+++ coderun2025_django/demo_app/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from .models import UserInfo, DemoModel
 # Create your views here.
+
+print(UserInfo.objects.all())
 def demo_app_page(request):
-    return render(request, 'demo_app/demo_app.html')
+    users = UserInfo.objects.all().order_by('-date') # order my newest
+
+    return render(request, 'demo_app/demo_app.html', context={'users' : users})
